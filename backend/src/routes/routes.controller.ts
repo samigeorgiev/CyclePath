4db5a9179fc83071a6f7345c8b64ce2cf857aab9
Anton Yanchev (A1 BG) <Anton.Yanchev@a1.bg>
2021-03-13 10:31:45 +0200

    [UPDATE] Add route for air pollutin in RouteContoller

--- backend/src/routes/routes.controller.ts
+++ backend/src/routes/routes.controller.ts
@@ -1,4 +1,6 @@
+import { Get } from '@nestjs/common'
 import { Body, Controller, HttpCode, HttpStatus, Post, ValidationPipe } from '@nestjs/common'
+import { AirPollutionReqDto } from './dto/air-pollution-req.dto'
 import { RateRouteDto } from './dto/rate-route.dto'
 import { RoutesService } from './routes.service'
 
@@ -6,12 +8,12 @@ import { RoutesService } from './routes.service'
 export class RoutesController {
     constructor(private readonly routesService: RoutesService) {}
 
-    get(
-        @Param('startNodeId') startNodeLat: number,
-        @Param('endNodeId') startNodeLong: number, 
-        @Param('startNodeId') startNodeLat: number,
-        @Param('endNodeId') startNodeLong: number, ) { 
-        }
+    // get(
+    //     @Param('startNodeId') startNodeLat: number,
+    //     @Param('endNodeId') startNodeLong: number, 
+    //     @Param('startNodeId') startNodeLat: number,
+    //     @Param('endNodeId') startNodeLong: number, ) { 
+    //     }
 
     @Post('rate')
     @HttpCode(HttpStatus.NO_CONTENT)
@@ -20,5 +22,11 @@ export class RoutesController {
         const userId: number = 1
 
         await this.routesService.rateRoute(rateRouteDto, userId)
+    }
+
+    @Get('air-pollution')
+    async airPollution (@Body(ValidationPipe) airPollutionReqDto: AirPollutionReqDto) {
+
+        return await this.routesService.airPollution(airPollutionReqDto)
     }
 }
--- backend/src/routes/routes.service.ts
+++ backend/src/routes/routes.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
+import { AirPollutionReqDto } from './dto/air-pollution-req.dto'
 import { RateRouteDto } from './dto/rate-route.dto'
 import { RouteRating } from './entities/route-rating.entity'
 import { Route } from './entities/route.entity'
 import { RoutesRatingRepository } from './repository/routes-rating.repository'
 import { RoutesRepository } from './repository/routes.repository'
+import fetch from 'node-fetch'
 
 @Injectable()
 export class RoutesService {
@@ -28,5 +30,14 @@ export class RoutesService {
         )
 
         await this.routesRepository.updateRouteRating(route.getId(), avgRating)
+    }
+
+    async airPollution(airPollutionReqDto: AirPollutionReqDto) {
+        const startPointUrl = `https://api.waqi.info/feed/geo:${airPollutionReqDto.startLat};${airPollutionReqDto.startLon}/?token=${process.env.AIR_QUALITY_KEY}`;
+        const endPointUrl = `https://api.waqi.info/feed/geo:${airPollutionReqDto.endLat};${airPollutionReqDto.endLon}/?token=${process.env.AIR_QUALITY_KEY}`;
+
+        await fetch (startPointUrl)
+            .then(res => res.json())
+            .then(console.log)
     }
 }
