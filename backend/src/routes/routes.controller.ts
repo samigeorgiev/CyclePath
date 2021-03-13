2343bd47d778827511db00bb12ced535dea44fd2
Toni Yanchev <anton.t.y2002@gmail.com>
2021-03-13 13:28:31 +0200

    [UPDATE] Finish air pollution endPoint

--- backend/src/routes/routes.controller.ts
+++ backend/src/routes/routes.controller.ts
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import { Get } from '@nestjs/common'
-import { Body, Controller, HttpCode, HttpStatus, Post, ValidationPipe } from '@nestjs/common'
-import { AirPollutionReqDto } from './dto/air-pollution-req.dto'
-=======
 import {
     Body,
     Controller,
@@ -13,8 +8,8 @@ import {
     Post,
     ValidationPipe
 } from '@nestjs/common'
+import { AirPollutionReqDto } from './dto/air-pollution-req.dto'
 import { GetRouteDto } from './dto/get-route.dto'
->>>>>>> 13e7ec72fa24686ab846ad45e394ce50ee9315fa
 import { RateRouteDto } from './dto/rate-route.dto'
 import { RoutesService } from './routes.service'
 
@@ -22,14 +17,6 @@ import { RoutesService } from './routes.service'
 export class RoutesController {
     constructor(private readonly routesService: RoutesService) {}
 
-<<<<<<< HEAD
-    // get(
-    //     @Param('startNodeId') startNodeLat: number,
-    //     @Param('endNodeId') startNodeLong: number, 
-    //     @Param('startNodeId') startNodeLat: number,
-    //     @Param('endNodeId') startNodeLong: number, ) { 
-    //     }
-=======
     @Get()
     get(
         @Param('startNodeId') startNodeLat: number,
@@ -40,7 +27,6 @@ export class RoutesController {
         const request = new GetRouteDto(startNodeLat, startNodeLong, endNodeLat, endNodeLong)
         // this.routesService.getRoute(request);
     }
->>>>>>> 13e7ec72fa24686ab846ad45e394ce50ee9315fa
 
     @Post('rate')
     @HttpCode(HttpStatus.NO_CONTENT)
--- backend/src/routes/routes.service.ts
+++ backend/src/routes/routes.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
-<<<<<<< HEAD
 import { AirPollutionReqDto } from './dto/air-pollution-req.dto'
-=======
 import { GetRouteDto } from './dto/get-route.dto'
->>>>>>> 13e7ec72fa24686ab846ad45e394ce50ee9315fa
 import { RateRouteDto } from './dto/rate-route.dto'
 import { Route } from './entities/route.entity'
 import { RoutesRatingRepository } from './repository/routes-rating.repository'
 import { RoutesRepository } from './repository/routes.repository'
 import fetch from 'node-fetch'
+import { AirPollutionRes } from './interfaces/air-pollution-res.interface'
 
 @Injectable()
 export class RoutesService {
@@ -35,12 +33,36 @@ export class RoutesService {
         await this.routesRepository.updateRouteRating(route.getId(), avgRating)
     }
 
-    async airPollution(airPollutionReqDto: AirPollutionReqDto) {
+    async getPollutionData(airPollutionReqDto: AirPollutionReqDto) {
         const startPointUrl = `https://api.waqi.info/feed/geo:${airPollutionReqDto.startLat};${airPollutionReqDto.startLon}/?token=${process.env.AIR_QUALITY_KEY}`;
         const endPointUrl = `https://api.waqi.info/feed/geo:${airPollutionReqDto.endLat};${airPollutionReqDto.endLon}/?token=${process.env.AIR_QUALITY_KEY}`;
 
-        await fetch (startPointUrl)
-            .then(res => res.json())
-            .then(console.log)
+        const startPointRes = await fetch (startPointUrl)
+        const startPointData = await startPointRes.json()
+
+        const endPointRes = await fetch (endPointUrl)
+        const endPointData = await endPointRes.json()
+
+        return { startPointData: startPointData, endPointData: endPointData }
+    }
+
+    async airPollution (airPollutionReqDto: AirPollutionReqDto) : Promise<AirPollutionRes> {
+        const data = await this.getPollutionData(airPollutionReqDto)
+        
+        let date;
+        if (new Date(data.startPointData.data.time.s).getTime() > new Date(data.endPointData.data.time.s).getTime()) {
+            date = data.startPointData.data.time.s;
+        } else {
+            date = data.endPointData.data.time.s;
+        }
+
+        const pollution = (data.startPointData.data.iaqi.pm10.v + data.endPointData.data.iaqi.pm10.v) / 2
+
+        
+        return {
+            measurementTime: date,
+            pollutionIndex: pollution
+        }
+
     }
 }
