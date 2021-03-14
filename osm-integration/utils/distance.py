3b990a773ba3255fc8c028c2dc3da37095ea6413
Victor Gorchilov <victor@gorchilov.com>
2021-03-14 04:11:15 +0200

    [up] create module utils

---
+++ osm-integration/utils/distance.py
@@ -0,0 +1,20 @@
+from math import asin, cos, radians, sin, sqrt
+
+
+def haversine(node_a, node_b):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(
+        radians, [node_a.long, node_a.lat, node_b.long, node_b.lat]
+    )
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
