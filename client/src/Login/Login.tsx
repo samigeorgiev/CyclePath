b38d5bc140694b272027029a7b64b0824824fe50
Valentin <valentinspasov9669@gmail.com>
2021-03-14 02:07:23 +0200

    FIX merge conflict

--- client/src/Login/Login.tsx
+++ client/src/Login/Login.tsx
@@ -1,65 +1,67 @@
-import React, { useContext, useState } from 'react'
-import { useHistory } from 'react-router-dom'
-import decode from 'jwt-decode'
-import LoginForm from '../components/LoginForm'
-import styles from './Login.module.scss'
-import { toast } from 'react-toastify'
-import { AuthContext } from 'src/context/Auth/AuthContext'
-import { AuthContextInterface } from 'src/context/Auth/AuthContext.interface'
-import { DecodedToken } from 'src/tokenTypes/DecodedToken'
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import decode from 'jwt-decode';
+import LoginForm from '../components/LoginForm';
+import styles from './Login.module.scss';
+import { toast } from 'react-toastify';
+import { AuthContextInterface } from '../context/Auth/AuthContext.interface';
+import { AuthContext } from '../context/Auth/AuthContext';
+import { DecodedToken } from '../tokenTypes/DecodedToken';
 
 interface Props {}
 
 const Login: React.FC<Props> = () => {
-  const [email, setEmail] = useState<string> ("");
-  const [password, setPassword] = useState<string> ("");
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
 
-  const { setAuthState } = useContext<AuthContextInterface>(AuthContext)
+    const { setAuthState } = useContext<AuthContextInterface>(AuthContext);
 
-  let history = useHistory()
+    let history = useHistory();
 
-  const login = () => {
-    fetch(`${process.env.REACT_APP_API_URL}/auth/sign-in`, {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      method: "POST",
-      body: JSON.stringify({
-        email: email,
-        password: password
-      })
-    })
-      .then(res => res.json())
-      .then(data => {
-        if (data.message) {
-          toast.error(data.message[0])
-          return
-        } else {
-          const { exp, id }: DecodedToken = decode(data.token)
-          setAuthState({
-            exp: exp * 1000 - Date.now(),
-            token: data.token,
-            userId: id
-          })
-          toast.success("Successful!")
-          history.push('/')
-        }
-      })
-  }
+    const login = () => {
+        fetch(`${process.env.REACT_APP_API_URL}/auth/sign-in`, {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.message) {
+                    toast.error(data.message[0]);
+                    return;
+                } else {
+                    const { exp, id }: DecodedToken = decode(data.token);
+                    setAuthState({
+                        exp: exp * 1000 - Date.now(),
+                        token: data.token,
+                        userId: id
+                    });
+                    toast.success('Successful!');
+                    history.push('/');
+                }
+            });
+    };
 
-  return (
-    <div className={styles.root}>
-      <LoginForm
-        email={email}
-        password={password}
-        handleEmail={e => setEmail(e.target.value)}
-        handlePassword={e => setPassword(e.target.value)}
-        handleLogin={() => login()}
-      />
-      <a href="/register" className={styles.link}>Register Now!</a>
-    </div>
-  )
-}
+    return (
+        <div className={styles.root}>
+            <LoginForm
+                email={email}
+                password={password}
+                handleEmail={(e) => setEmail(e.target.value)}
+                handlePassword={(e) => setPassword(e.target.value)}
+                handleLogin={() => login()}
+            />
+            <a href='/register' className={styles.link}>
+                Register Now!
+            </a>
+        </div>
+    );
+};
 
-export default Login
+export default Login;
--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,47 +1,50 @@
-import React, { useContext } from 'react'
-import { NavLink } from 'react-router-dom'
-import { AuthContext } from 'src/context/Auth/AuthContext'
-import { AuthContextInterface } from 'src/context/Auth/AuthContext.interface'
-import styles from './Nav.module.scss'
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../../context/Auth/AuthContext';
+import { AuthContextInterface } from '../../context/Auth/AuthContext.interface';
+
+import styles from './Nav.module.scss';
 
 interface Props {}
 
 const Nav: React.FC<Props> = () => {
-    const { authState } = useContext<AuthContextInterface>(AuthContext)
+    const { authState } = useContext<AuthContextInterface>(AuthContext);
 
     const authRoutes = [
         { name: 'Home', path: '/' },
-        { name: 'Map', path: '/map' },
-    ]
+        { name: 'Map', path: '/map' }
+    ];
     const noAuthRoutes = [
         { name: 'Home', path: '/' },
         { name: 'Login', path: '/login' }
-    ]
+    ];
     return (
         <div className={styles.root}>
-            {authState ? authRoutes.map((route) => (
-                <NavLink
-                    exact
-                    to={route.path}
-                    className={styles.link}
-                    activeClassName={styles.active}
-                    key={route.name.toLowerCase()}
-                >
-                    {route.name}
-                </NavLink>)
-            ) : noAuthRoutes.map((route) => (
-                <NavLink
-                    exact
-                    to={route.path}
-                    className={styles.link}
-                    activeClassName={styles.active}
-                    key={route.name.toLowerCase()}
-                >
-                    {route.name}
-                </NavLink>)
-            )}
+            {authState
+                ? authRoutes.map((route) => (
+                      <NavLink
+                          exact
+                          to={route.path}
+                          className={styles.link}
+                          activeClassName={styles.active}
+                          key={route.name.toLowerCase()}
+                      >
+                          {route.name}
+                      </NavLink>
+                  ))
+                : noAuthRoutes.map((route) => (
+                      <NavLink
+                          exact
+                          to={route.path}
+                          className={styles.link}
+                          activeClassName={styles.active}
+                          key={route.name.toLowerCase()}
+                      >
+                          {route.name}
+                      </NavLink>
+                  ))}
         </div>
-    )
-}
+    );
+};
 
-export default Nav
+export default Nav;
