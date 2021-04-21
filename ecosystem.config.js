4a16fda5b9beea86151431625b22d8ee9a9e86c0
Victor Gorchilov <victor@gorchilov.com>
2021-04-21 11:03:27 +0300

    [fix] rename backend to server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'cyclepath-backend',
-            script: './backend/dist/main.js',
+            script: './server/dist/main.js',
             env: {
                 PORT: 5000,
                 NODE_ENV: 'production',
