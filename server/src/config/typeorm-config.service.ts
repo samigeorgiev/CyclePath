6eb68f29342b9ac107b59d797b7e2972132bceca
Victor Gorchilov <victor@gorchilov.com>
2021-04-21 11:20:48 +0300

    [up] disable typeorm logging in production

--- server/src/config/typeorm-config.service.ts
+++ server/src/config/typeorm-config.service.ts
@@ -4,6 +4,8 @@ import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm'
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
     createTypeOrmOptions(): TypeOrmModuleOptions {
+        const inProd = process.env.NODE_ENV === 'production'
+
         return {
             type: 'postgres',
             host: process.env.PG_HOST,
@@ -11,9 +13,9 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
             username: process.env.PG_USER,
             password: process.env.PG_PASSWORD,
             database: process.env.PG_DB,
-            synchronize: true,
             dropSchema: false,
-            logging: true,
+            synchronize: !inProd,
+            logging: !inProd,
             entities: [__dirname + '/../**/*.entity.{js,ts}']
         }
     }
