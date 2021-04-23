0d26861a7d373165079e2d19f16b8ad00c431105
Victor Gorchilov <victor@gorchilov.com>
2021-04-23 11:09:37 +0300

    [fix] search disables map

--- client/src/components/Search/Search.tsx
+++ client/src/components/Search/Search.tsx
@@ -1,14 +1,15 @@
 import {
     CircularProgress,
+    ClickAwayListener,
     InputAdornment,
     List,
-    ListItem,
     ListItemText,
     TextField
 } from '@material-ui/core'
 import { Search as SearchIcon } from '@material-ui/icons'
 import { LatLngLiteral } from 'leaflet'
-import React, { Dispatch, SetStateAction, useState } from 'react'
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { useMap } from 'react-leaflet'
 import styles from './Search.module.scss'
 
 interface Props {
@@ -25,6 +26,9 @@ const Search: React.FC<Props> = (props) => {
     const [search, setSearch] = useState('')
     const [results, setResults] = useState<Result[]>([])
     const [isLoading, setIsLoading] = useState(false)
+    const [isDisabled, setIsDisabled] = useState(false)
+
+    const map = useMap()
 
     const getDestinationFromSearch = (search: string) => {
         if (!search.trim() || !props.position) return
@@ -43,70 +47,92 @@ const Search: React.FC<Props> = (props) => {
             })
             .then((data) => {
                 setResults(data)
+                setIsDisabled(true)
             })
-            .finally(() => setIsLoading(false))
+            .finally(() => {
+                setIsLoading(false)
+            })
     }
 
     const clickHandler = (result: any) => {
         setSearch('')
         setResults([])
+        setIsDisabled(false)
+
         props.setDestination({
             lat: result.lat,
             lng: result.lng
         })
     }
 
+    useEffect(() => {
+        if (isDisabled) {
+            map.scrollWheelZoom.disable()
+            return
+        }
+
+        map.scrollWheelZoom.enable()
+    }, [isDisabled])
+
     return (
-        <form
-            onSubmit={(e) => {
-                e.preventDefault()
-                getDestinationFromSearch(search)
-            }}
-            className={styles.container}
-        >
-            <TextField
-                id='input-with-icon-textfield'
-                value={search}
-                variant='outlined'
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    setSearch(e.target.value)
-                }}
-                autoComplete='off'
-                fullWidth
-                className={styles.input}
-                InputProps={{
-                    startAdornment: (
-                        <InputAdornment position='start'>
-                            <SearchIcon color='primary' />
-                        </InputAdornment>
-                    ),
-                    endAdornment: (
-                        <InputAdornment position='end'>
-                            {isLoading && (
-                                <CircularProgress
-                                    color='primary'
-                                    size='1.5em'
-                                />
-                            )}
-                        </InputAdornment>
-                    )
-                }}
-            />
-            <List className={styles.results}>
-                {results.map((result) => (
-                    <ListItem
-                        button
-                        key={`${result.lat},${result.lng}`}
-                        onClick={() => clickHandler(result)}
+        <>
+            <ClickAwayListener onClickAway={() => setIsDisabled(false)}>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault()
+                        getDestinationFromSearch(search)
+                    }}
+                    className={styles.container}
+                >
+                    <TextField
+                        id='input-with-icon-textfield'
+                        value={search}
+                        variant='outlined'
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            setSearch(e.target.value)
+                        }}
+                        autoComplete='off'
+                        fullWidth
+                        className={styles.input}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position='start'>
+                                    <SearchIcon color='primary' />
+                                </InputAdornment>
+                            ),
+                            endAdornment: (
+                                <InputAdornment position='end'>
+                                    {isLoading && (
+                                        <CircularProgress
+                                            color='primary'
+                                            size='1.5em'
+                                        />
+                                    )}
+                                </InputAdornment>
+                            )
+                        }}
+                    />
+                    <List
+                        className={[
+                            styles.results,
+                            isDisabled && styles.active
+                        ].join(' ')}
                     >
-                        <ListItemText
-                            primary={result.name}
-                            secondary={result.address}
-                        />
-                    </ListItem>
-                ))}
-            </List>
-        </form>
+                        {results.map((result) => (
+                            <ListItemText
+                                key={`${result.lat},${result.lng}`}
+                                onClick={() => clickHandler(result)}
+                            >
+                                <ListItemText
+                                    primary={result.name}
+                                    secondary={result.address}
+                                />
+                            </ListItemText>
+                        ))}
+                    </List>
+                </form>
+            </ClickAwayListener>
+        </>
     )
 }
 
